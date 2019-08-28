2b819f474007ebe2f1b4ef302f746b6aa2db4c63
kzoacn <kzoacn@outlook.com>
2019-08-28 00:03:50 -0500

    fix

--- ec_group/group.cpp
+++ ec_group/group.cpp
@@ -25,14 +25,16 @@ void Group::mul(Point &res, const Point &lhs, const BigInt &m)
 {
     EC_POINT_mul(ec_group, res.p, NULL, lhs.p, m.n, NULL);
 }
-char* Group::to_hex(char* s,const Point &p) const
+char* Group::to_hex(const Point &p) const
 {
     BigInt x, y;
     EC_POINT_get_affine_coordinates_GFp(ec_group, p.p, x.n, y.n, NULL);
     char *sx,*sy;
     sx=x.to_hex();
     sy=y.to_hex();
-    char *res=new char[4+strlen(sx)+strlen(sy)];
+    int len=3+strlen(sx)+strlen(sy);
+    char *res=new char[len+1];
+    memset(res,0,len+1);
     strcat(res,"(");
     strcat(res,sx);
     strcat(res,",");
@@ -45,12 +47,17 @@ void Group::from_hex(Point &p,const char *s) const
 {
     BigInt x, y;
     int len=strlen(s);
-    char *t;
+    char *t=new char[len+1];
+    memset(t,0,len+1);
+    t[len]=0;
     strcpy(t,s);
     for(int i=0;i<len;i++){
         if(s[i]==','){
-            x.from_hex(s.substr(1,i));
-            y.from_hex(s.substr(i+1,s.length()-i-1));
+            t[0]=0;
+            t[i]=0;
+            t[len-1]=0;
+            x.from_hex(t+1);
+            y.from_hex(t+i+1);
         }
     }
     EC_POINT_set_affine_coordinates_GFp(ec_group,p.p,x.n,y.n,NULL);
--- test/ot.cpp
+++ test/ot.cpp
@@ -5,18 +5,21 @@ using namespace std;
 using namespace emp;
 
 //stupid hash
-string H(const Group &G,const Point &p){
+string H(const Group &G, const Point &p)
+{
     return G.to_hex(p);
 }
 
-string E(string key,string m){
-    for(int i=0;i<(int)m.length();i++)
-        m[i]^=key[i];
+string E(string key, string m)
+{
+    for (int i = 0; i < (int)m.length(); i++)
+        m[i] ^= key[i];
     return m;
 }
-string D(string key,string c){
-    for(int i=0;i<(int)c.length();i++)
-        c[i]^=key[i];
+string D(string key, string c)
+{
+    for (int i = 0; i < (int)c.length(); i++)
+        c[i] ^= key[i];
     return c;
 }
 
@@ -35,43 +38,44 @@ void recv_string(NetIO *io, const Group &G, string &As)
     int len;
     io->recv_data(&len, 4);
     io->recv_data(tmp, len);
-    As=string(tmp);
+    As = string(tmp);
 }
-
 void send_point(NetIO *io, const Group &G, const Point &A)
 {
-
-    string As;
-    As = G.to_hex(A);
-    int len = As.length();
+    char *data = G.to_hex(A);
+    int len = strlen(data);
     io->send_data(&len, 4);
-    io->send_data(As.c_str(), len);
+    io->send_data(data, len);
 }
 
 void recv_point(NetIO *io, const Group &G, Point &A)
 {
 
-    char tmp[256];
-    memset(tmp, 0, sizeof tmp);
     int len;
+    char *data;
     io->recv_data(&len, 4);
-    io->recv_data(tmp, len);
-    G.from_hex(A, string(tmp));
+    data = new char[len + 1];
+    data[len] = 0;
+    io->recv_data(data, len);
+    G.from_hex(A, data);
 }
 
-string ot(NetIO *io,int party,string m[],int c){
-    BigInt a,b;
-
-    if(party==ALICE){
+string ot(NetIO *io, int party, string m[], int c)
+{
+    BigInt a, b;
+    if (party == ALICE)
+    {
         a.from_dec("324123");
-    }else{
+    }
+    else
+    {
         b.from_dec("4134213");
     }
-    
+
     //stupid random
-    
+
     Group G(NID_secp256k1);
-    Point A,B;
+    Point A, B;
     Point g;
     G.init(A);
     G.init(B);
@@ -79,90 +83,98 @@ string ot(NetIO *io,int party,string m[],int c){
     G.get_generator(g);
 
 
-
-    if(party==ALICE){
-        A=g;
-        G.mul(A,A,a);
+    if (party == ALICE)
+    {
+        A = g;
+        G.mul(A, A, a);
         //send A
         send_point(io,G,A);
-    }else{
+    }
+    else
+    {
         recv_point(io,G,A);
     }
 
-
     string kb;
-    if(party==BOB){
-        if(c==0){
-            B=g;
-            G.mul(B,B,b);
-        }else{
-            B=g;
-            G.mul(B,B,b);
-            G.add(B,B,A);
+    if (party == BOB)
+    {
+        if (c == 0)
+        {
+            B = g;
+            G.mul(B, B, b);
+        }
+        else
+        {
+            B = g;
+            G.mul(B, B, b);
+            G.add(B, B, A);
         }
         //send B
-        send_point(io,G,B);
-
+        send_point(io, G, B);
 
         Point t0;
         G.init(t0);
-        t0=A;
-        G.mul(t0,t0,b);
-        kb=H(G,t0);
-
-    }else{
-        recv_point(io,G,B);
+        t0 = A;
+        G.mul(t0, t0, b);
+        kb = H(G, t0);
+    }
+    else
+    {
+        recv_point(io, G, B);
     }
     string e[2];
-    if(party==ALICE){
-        Point t1,t2,iv;
+
+    if (party == ALICE)
+    {
+        Point t1, t2, iv;
         G.init(t1);
         G.init(t2);
         G.init(iv);
-        t1=B;
-        G.mul(t1,t1,a);
+        t1 = B;
+        G.mul(t1, t1, a);
 
-        
-        
-        t2=B;
+        t2 = B;
 
-        iv=A;
-        G.inv(iv,iv);
-        G.add(t2,t2,iv);
+        iv = A;
+        G.inv(iv, iv);
+        G.add(t2, t2, iv);
 
-        G.mul(t2,t2,a);
-        string k0=H(G,t1);
-        string k1=H(G,t2);
-        e[0]=E(k0,m[0]);
-        e[1]=E(k1,m[1]);
+        G.mul(t2, t2, a);
+        string k0 = H(G, t1);
+        string k1 = H(G, t2);
+        e[0] = E(k0, m[0]);
+        e[1] = E(k1, m[1]);
 
-        send_string(io,G,e[0]);
-        send_string(io,G,e[1]);
+        send_string(io, G, e[0]);
+        send_string(io, G, e[1]);
         return "";
-    }else{
-        recv_string(io,G,e[0]);
-        recv_string(io,G,e[1]);
-        return D(kb,e[c]);
+    }
+    else
+    {
+        recv_string(io, G, e[0]);
+        recv_string(io, G, e[1]);
+        return D(kb, e[c]);
     }
 }
 
-int main(int argc, char** argv){
+int main(int argc, char **argv)
+{
 
-    if(argc<2){
-        cout<<"./ot <party>";
+    if (argc < 2)
+    {
+        cout << "./ot <party>";
         exit(0);
-    }    
+    }
 
-    int party,port=12345;
-    sscanf(argv[1],"%d",&party);
-	NetIO * io = new NetIO(party==ALICE ? nullptr : "127.0.0.1", port);
+    int party, port = 12345;
+    sscanf(argv[1], "%d", &party);
+    NetIO *io = new NetIO(party == ALICE ? nullptr : "127.0.0.1", port);
 
     string m[2];
-    m[0]="hello";
-    m[1]="goodbye";
+    m[0] = "hello";
+    m[1] = "goodbye";
 
-    cout<<ot(io,party,m,0)<<endl;
-
+    cout << ot(io, party, m, 1) << endl;
 
     return 0;
 }
